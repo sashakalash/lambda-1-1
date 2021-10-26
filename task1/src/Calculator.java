9c4d369712c8d198a53e41c84c6b81562b1955d1
Aleksandr Koloshtivin <aykolosh@mts.ru>
2021-10-26 09:13:25 +0300

    Добавлено возвращение Integer.MAX_INT при некорректной операции деления на ноль

---
+++ task1/src/Calculator.java
@@ -0,0 +1,20 @@
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    // причина ошибки в делении на ноль, решается проверкой второй переменной
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y == 0) {
+            System.out.println("На ноль делить нельзя!");
+            return Integer.MAX_INT;
+        }
+        return x / y;
+    };
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+}
